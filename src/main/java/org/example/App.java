d91d6cc7374c91ee08d992ebb9e3f1285db910e2
vladislavzelinskij <zelya.tt@gmail.com>
2025-01-02 21:04:53 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+public class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Actions!");
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.App;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3), "2 + 3 should equal 5");
+    }
+}
